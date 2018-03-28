c41e9d50a2d9c6a7d63201e2a370665385c101c8
G00306474 <G00306474@gmit.ie>
2018-03-28 23:14:25 +0100

    Basic shunting algo done

--- shunt.go
+++ shunt.go
@@ -4,16 +4,45 @@ package main
 	"fmt"
  )
 
-
  func intopost(infix string) string {
 
 	specials := map[rune]int{'*':10,'.':9,'|':8}
 
-
 	pofix := []rune{}
 	s := []rune{}
 
-	return string(pofix)
+	for _, r:= range infix{
+		switch{
+
+		case r == '(':// if we read an open bracket we will add it to the stack 
+			s = append(s,r)
+		case r == ')':// if we read an closing bracket we will remove everything till we hit an open bracket on the stack
+			for s[len(s)-1] != '('{
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]//takes everything but the last charcter and makes it equal to s 
+			}
+			s = s[:len(s)-1]//discards open bracket
+		case specials [r] > 0: //gets the value 0 back if not in specials 
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				pofix = append(pofix, s[len(s)-1])
+				s = s[:len(s)-1]
+			}
+			s = append(s, r)
+
+		default:
+
+			pofix = append(pofix, r)
+		}
+
+	}
+	for len(s) > 0 {
+		
+			pofix = append(pofix, s[len(s)-1])
+			s = s[:len(s)-1]// puts top element of stack and puts it in pofix
+	
+
+	}
+	return string(pofix)//changes rune back  to string 
  }
 
  func main(){
@@ -29,9 +58,9 @@ package main
 	fmt.Println("Infix:   ", "a.(b|d)c*")
 	fmt.Println("Postfix:   ",intopost( "a.(b|d)c*"))
 
-	//Answer: abb.+.c*.
-	fmt.Println("Infix:   ", "a.(b.b)+.c*")
-	fmt.Println("Postfix:   ",intopost( "a.(b.b)+.c*"))
+	//Answer: abb.+.c.
+	fmt.Println("Infix:   ", "a.(b.b)+.c")
+	fmt.Println("Postfix:   ",intopost( "a.(b.b)+.c"))
 
 	
  }
